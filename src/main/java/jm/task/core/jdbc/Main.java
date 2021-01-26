0059976b7eb31ffffa5e7fe444b91b3eb9ab5f61
popperro-001 <talkin.watermelion@gmail.com>
2021-01-26 21:46:05 +0900

    for 1.1.3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,5 +1,6 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
@@ -7,12 +8,12 @@ import org.hibernate.Session;
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
-        UserServiceImpl userService = new UserServiceImpl();
-        userService.createUsersTable();
+        UserService userService = new UserServiceImpl();
+        //userService.createUsersTable();
         userService.saveUser("Tom", "White", (byte)23);
         userService.saveUser("Jack", "Black", (byte)45);
         userService.saveUser("Nick", "Red", (byte)36);
-        userService.saveUser("Bob", "Yellow", (byte)60);
+        //userService.saveUser("Rob", "Green", (byte)28);
         //userService.removeUserById(2);
         //userService.cleanUsersTable();
 
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -10,32 +10,32 @@ public class UserServiceImpl implements UserService {
     UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
     UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
     public void createUsersTable() {
-        //userDaoJDBC.createUsersTable();
-        userDaoHibernate.createUsersTable();
+        userDaoJDBC.createUsersTable();
+        //userDaoHibernate.createUsersTable();
     }
 
     public void dropUsersTable() {
-        //userDaoJDBC.dropUsersTable();
-        userDaoHibernate.dropUsersTable();
+        userDaoJDBC.dropUsersTable();
+        //userDaoHibernate.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        //userDaoJDBC.saveUser(name, lastName, age);
-        userDaoHibernate.saveUser(name, lastName, age);
+        userDaoJDBC.saveUser(name, lastName, age);
+        //userDaoHibernate.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        //userDaoJDBC.removeUserById(id);
-        userDaoHibernate.removeUserById(id);
+        userDaoJDBC.removeUserById(id);
+        //userDaoHibernate.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        //return userDaoJDBC.getAllUsers();
-        return userDaoHibernate.getAllUsers();
+        return userDaoJDBC.getAllUsers();
+        //return userDaoHibernate.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        //userDaoJDBC.cleanUsersTable();
-        userDaoHibernate.cleanUsersTable();
+        userDaoJDBC.cleanUsersTable();
+        //userDaoHibernate.cleanUsersTable();
     }
 }
